068cc3e872241c4d26400aac1ebbcf93182c663b
Ahmed Abououf <aabuauf@yahoo.com>
2019-07-21 18:36:39 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,5 +1,6 @@
 
 class Book
+  attr_accessors :author, :page_count, :genre
   def initialize (title)
     @title = title
   end
@@ -7,29 +8,7 @@ class Book
     @title
   end
   
-  def author=(author)
-    @author= author
-  end
-  
-  def author
-    @author
-  end
-  
-  def page_count=(page_count)
-    @author= page_count
-  end
-  
-  def page_count
-    @author
-  end
-  
-  def genre=(genre)
-    @genre= genre
-  end
-  
-  def genre
-    @genre
-  end
+ 
   
   def turn_page
     puts "Flipping the page...wow, you read fast!"
